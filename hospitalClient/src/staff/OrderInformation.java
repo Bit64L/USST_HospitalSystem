2c7ce1dc7ecb30575aa7552b6de4f7f3e4adeef4
hylusst <13738849868@163.com>
2016-12-21 22:03:14 +0800

    添加server端方法

--- hospitalClient/src/staff/OrderInformation.java
+++ hospitalClient/src/staff/OrderInformation.java
@@ -13,7 +13,7 @@ public class OrderInformation  implements Serializable{
 	private String patientName;
 	private String patientAge;
 	private String patientSex;
-	private String patientPN;
+	private String patientPhoneNumber;
 	private HospitalDepartment hospitalDepartment;
 	private String ordertime;
 	
@@ -37,7 +37,7 @@ public class OrderInformation  implements Serializable{
 		this.setPatientName(patient.getName());
 		this.setPatientAge(patient.getAge());
 		this.setPatientSex(patient.getSex());
-		this.setPatientPN(patient.getPhoneNumber());
+		this.setPatientPhoneNumber(patient.getPhoneNumber());
 		this.setHospitalDepartment(hospitalDepartment);
 		this.setOrdertime(ordertime);
 	}
@@ -76,12 +76,12 @@ public class OrderInformation  implements Serializable{
 		this.patientSex = patientSex;
 	}
 
-	public String getPatientPN() {
-		return patientPN;
+	public String getPatientPhoneNumber() {
+		return patientPhoneNumber;
 	}
 
-	public void setPatientPN(String patientPN) {
-		this.patientPN = patientPN;
+	public void setPatientPhoneNumber(String patientPhoneNumber) {
+		this.patientPhoneNumber = patientPhoneNumber;
 	}
 
 
